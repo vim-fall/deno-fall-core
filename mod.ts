f54f6775ed0a1bb236a80678e37f3bc5b600d5ea
Alisue <lambdalisue@gmail.com>
2024-03-14 03:38:53 +0900

    :bug: Export types in `item.ts`

--- mod.ts
+++ mod.ts
@@ -5,9 +5,10 @@
  * [Denops]: https://github.com/vim-denops/denops.vim
  * @module
  */
-export * from "./action.ts";
-export * from "./filter.ts";
-export * from "./previewer.ts";
-export * from "./renderer.ts";
-export * from "./sorter.ts";
-export * from "./source.ts";
+export type * from "./action.ts";
+export type * from "./filter.ts";
+export type * from "./item.ts";
+export type * from "./previewer.ts";
+export type * from "./renderer.ts";
+export type * from "./sorter.ts";
+export type * from "./source.ts";
