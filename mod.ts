e3bc6632d8adb8e048ed98e9720b48f1a6e4524f
Alisue <lambdalisue@gmail.com>
2024-05-10 17:54:08 +0900

    :boom: Refine `Filter/Sorter` to `Transformer/Projector`

--- mod.ts
+++ mod.ts
@@ -6,9 +6,9 @@
  * @module
  */
 export type * from "./action.ts";
-export type * from "./filter.ts";
 export type * from "./item.ts";
 export type * from "./previewer.ts";
+export type * from "./projector.ts";
 export type * from "./renderer.ts";
-export type * from "./sorter.ts";
 export type * from "./source.ts";
+export type * from "./transformer.ts";
