5300f03f7f27291e7d046e4a043f1fcd144f6077
Alisue <lambdalisue@gmail.com>
2024-05-11 14:38:21 +0900

    :+1: Add `utils`

--- mod.ts
+++ mod.ts
@@ -5,10 +5,5 @@
  * [Denops]: https://github.com/vim-denops/denops.vim
  * @module
  */
-export type * from "./action.ts";
-export type * from "./item.ts";
-export type * from "./previewer.ts";
-export type * from "./projector.ts";
-export type * from "./renderer.ts";
-export type * from "./source.ts";
-export type * from "./transformer.ts";
+export type * from "./types.ts";
+export * from "./utils.ts";
---
+++ types.ts
@@ -0,0 +1,7 @@
+export type * from "./action.ts";
+export type * from "./item.ts";
+export type * from "./previewer.ts";
+export type * from "./projector.ts";
+export type * from "./renderer.ts";
+export type * from "./source.ts";
+export type * from "./transformer.ts";
